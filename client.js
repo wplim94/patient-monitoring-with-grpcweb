1a92632aeae9b740d1b4bff7bd743efb276efb75
Lim Wan Ping <wp@bondlinc.com>
2019-06-09 01:14:50 +0800

    implement server and client with service method publishData

---
+++ client.js
@@ -0,0 +1,25 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+const PROTO_PATH = "vital_signs.proto";
+const SERVER_URI = "0.0.0.0:50051";
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+const device = new protoDescriptor.VitalSignsService(
+  SERVER_URI,
+  grpc.credentials.createInsecure()
+);
+
+device.publishData(
+  { heartRate: 1, bloodPressure: 2, temperature: 3, deviceId: "VSID0001" },
+  (error, response) => {
+    if (error) {
+      console.error(error);
+    }
+    if (response) {
+      console.log(response);
+    }
+  }
+);
---
+++ server.js
@@ -0,0 +1,21 @@
+const grpc = require('grpc')
+const protoLoader = require('@grpc/proto-loader')
+
+const PROTO_PATH = 'vital_signs.proto'
+const SERVER_URI = '0.0.0.0:50051'
+
+const publishData = (call, callback) => {
+    console.log("Received data : ", call);
+}
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH)
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+
+const server = new grpc.Server()
+server.addService(protoDescriptor.VitalSignsService.service, {
+    publishData: publishData
+})
+server.bind(SERVER_URI, grpc.ServerCredentials.createInsecure())
+
+server.start()
+console.log('Server is running!')
