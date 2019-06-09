bf4a874c766facc79aad251840c82bad598f30b1
Lim Wan Ping <wp@bondlinc.com>
2019-06-09 18:35:03 +0800

    add service methods so that patient device can register itself in the server; simulate vital signs and send data every second

--- client.js
+++ client.js
@@ -12,9 +12,61 @@ const device = new protoDescriptor.VitalSignsService(
   grpc.credentials.createInsecure()
 );
 
+// assign patient device an UUID
+const util = require('./util.js')
+const deviceId = util.uuidV4();
+
+// define the normal range of vital signs
+const HEART_RATE_RAND = {
+  min: 60,
+  max: 120
+};
+
+const BLOOD_PRESSURE_RAND = {
+  min: 100,
+  max: 200
+};
+
+const TEMPERATURE_RAND = {
+  min: 25,
+  max: 45
+};
+
+// function to get random value within a range
+getRandValue = range => {
+  return Math.random() * (range.max - range.min) + range.min;
+};
+
+// function to simulate vital signs values
+const simulateVitalSigns = () => {
+  this.heartRateVal = getRandValue(HEART_RATE_RAND);
+  this.temperatureVal = getRandValue(TEMPERATURE_RAND);
+  this.bloodPressureVal = getRandValue(BLOOD_PRESSURE_RAND);
+}
+
+device.registerDevice({
+  deviceId: deviceId
+}, (error, response) => {
+  if (error) {
+    console.error(error);
+  }
+  if (response) {
+    console.log(response);
+  }
+})
+
+// function to publish vital signs to server
 const publishData = () => {
+  simulateVitalSigns();
+
   device.publishData(
-    { heartRate: 1, bloodPressure: 2, temperature: 3, deviceId: "VSID0001", timestampUnix: (new Date).getTime() },
+    {
+      heartRate: this.heartRateVal,
+      bloodPressure: this.bloodPressureVal,
+      temperature: this.temperatureVal,
+      deviceId: deviceId,
+      timestampUnix: new Date().getTime()
+    },
     (error, response) => {
       if (error) {
         console.error(error);
@@ -24,6 +76,9 @@ const publishData = () => {
       }
     }
   );
-}
+};
 
-publishData();
+// publish data every second
+setInterval(() => {
+  publishData();
+}, 1000);
--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const protoLoader = require('@grpc/proto-loader')
 const PROTO_PATH = 'vital_signs.proto'
 const SERVER_URI = '0.0.0.0:50051'
 
+const devices = [];
+
+const registerDevice = (call, callback) => {
+    console.log("Registered device : ", call.request);
+    devices.push(call.request.deviceId);
+    return callback(null, {
+        message: `Successfully registered device ${call.request.deviceId}` 
+    })
+}
+
 const publishData = (call, callback) => {
     console.log("Received data : ", call.request);
 }
@@ -13,6 +23,7 @@ const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
 
 const server = new grpc.Server()
 server.addService(protoDescriptor.VitalSignsService.service, {
+    registerDevice: registerDevice,
     publishData: publishData
 })
 server.bind(SERVER_URI, grpc.ServerCredentials.createInsecure())
